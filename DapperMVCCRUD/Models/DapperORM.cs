8c73fa4d5688e91897c2d265e0481c9603d734a1
gfernandezi <giancarlo.fernandez@gmail.com>
2017-09-14 21:54:46 -0500

    Agregar archivos de proyecto.

---
+++ DapperMVCCRUD/Models/DapperORM.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace DapperMVCCRUD.Models
+{
+    public static class DapperORM
+    {
+        //private static string connectionString = @"Data Source=(local)\sqle2012;Initial Catalog=DapperDB;Integrated Security=True;";
+        private static string connectionString = ConfigurationManager.ConnectionStrings["CNDapperDB"].ConnectionString;
+
+        
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        
+        }
+
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure),typeof(T));
+            }
+
+        }
+
+        //DapperORM.ReturnList<EmployeeModel> <=  IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
